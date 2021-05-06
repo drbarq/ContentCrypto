b78f64fd0fde1aeb4c98d5f88796f55f825525d0
Joe Tustin <J.Tustin@gmail.com>
2021-05-06 13:13:06 -0600

    updating useEffect

--- src/components/BTCPrice.tsx
+++ src/components/BTCPrice.tsx
@@ -9,39 +9,75 @@ interface SidebarProps {
 }
 const CONTENT_FIELD_ID = 'body';
 
-const btcPrice = () => {
-    let curRate
-    let curTime
 
-    axios.get('https://rest.coinapi.io/v1/exchangerate/BTC?apikey=600798C7-84CB-4842-B476-3D4427243952')
-        .then(function (response) {
-            curRate = response.data.rates[3326].rate
-            curTime = response.data.rates[3326].time
-            // handle success
-            console.log("Hello");
-            console.log(response);
-            console.log(response.data.rates[3326]);
-            // return {
-            //     curRate, curTime
-            // }
-        })
-        .catch(function (error) {
-            // handle error
-            console.log(error);
-        })
-        .then(function () {
-            // always executed
-    });
 
-        console.log(curTime, "curTime")
-        console.log(curRate, "curRate")
 
-    // return [
-    //     curRate, 
-    //     curTime
-    // ]
-    return '$54,454'
-}
+// async function btcPrice2() {
+//     let curRate
+//     let curTime
+
+//     try {
+//       const response = await axios.get('https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=600798C7-84CB-4842-B476-3D4427243952')
+//       curRate = response.data.rate
+//       curTime = response.data.time
+//     //   curRate = response.data.rates[3326].rate
+//     //   curTime = response.data.rates[3326].time
+//     //   console.log(response);
+//       console.log(curRate, "curRate")
+//       console.log(curTime, "curTime")
+//     } catch (error) {
+//       console.error(error);
+//     }
+
+//     // return '$54,454'
+
+//     curStats = "yes"
+
+
+//     return {
+//         curRate, 
+//         curTime
+//     }
+
+//   }
+
+
+
+
+// const btcPrice = () => {
+//     let curRate
+//     let curTime
+
+//     axios.get('https://rest.coinapi.io/v1/exchangerate/BTC?apikey=600798C7-84CB-4842-B476-3D4427243952')
+//         .then(function (response) {
+//             curRate = response.data.rates[3326].rate
+//             curTime = response.data.rates[3326].time
+//             // handle success
+//             // console.log("Hello");
+//             // console.log(response);
+//             // console.log(response.data.rates[3326]);
+//             // return {
+//             //     curRate, curTime
+//             // }
+//         })
+//         .catch(function (error) {
+//             // handle error
+//             console.log(error);
+//         })
+//         .then(function () {
+//             // always executed
+//     });
+
+//         console.log(curTime, "curTime")
+//         console.log(curRate, "curRate")
+
+//     // return [
+//     //     curRate, 
+//     //     curTime
+//     // ]
+//     // return '$54,454'
+//     return curRate
+// }
 
 
 
@@ -62,9 +98,26 @@ const Sidebar = (props: SidebarProps) => {
 //   }, [contentField]);
 
     useEffect(() => {
-        setBtcStats(btcPrice())
-        console.log(`hello, btcStats ${btcStats}`)
-    }, [btcStats])
+        let curRate
+        let curTime
+
+        axios.get('https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=600798C7-84CB-4842-B476-3D4427243952')
+            .then(response=> {
+                curRate = response.data.rate
+                curTime = response.data.time
+            })
+   
+            //   curRate = response.data.rates[3326].rate
+            //   curTime = response.data.rates[3326].time
+            //   console.log(response);
+            console.log(curRate, "curRate")
+            console.log(curTime, "curTime")
+       
+    
+        // setBtcStats(curStats)
+        console.log(`hello, btcStats ${curRate} ${curTime}`)
+    }, [])
+
 
   // Calculate the metrics based on the new value
 //   const stats = readingTime(blogText || '');
@@ -77,7 +130,8 @@ const Sidebar = (props: SidebarProps) => {
       <Note style={{ marginBottom: '12px' }}>
         Current BTC Price:
         <List style={{ marginTop: '12px' }}>
-            {btcPrice()}
+            {/* {curStats} */}
+            "Hello"
         </List>
       </Note>
     </>
