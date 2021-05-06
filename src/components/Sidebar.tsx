ab269ff08799197b0a81e3faaf961c4c52c215d5
Joe Tustin <J.Tustin@gmail.com>
2021-05-06 11:57:19 -0600

    adding in sidebar metrics

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Paragraph } from '@contentful/forma-36-react-components';
 import { SidebarExtensionSDK } from '@contentful/app-sdk';
 
@@ -6,7 +6,19 @@ interface SidebarProps {
   sdk: SidebarExtensionSDK;
 }
 
+// The field ID from our blog post field
+const CONTENT_FIELD_ID = 'body';
+
 const Sidebar = (props: SidebarProps) => {
+  // The sdk allows us to interact with the Contentful web app
+  const { sdk } = props;
+
+  // With the field ID we can reference individual fields from an entry
+  const contentField = sdk.entry.fields[CONTENT_FIELD_ID];
+
+  // Get the current value from the blog post field and store it in React state
+  const [blogText, setBlogText] = useState(contentField.getValue());
+
   return <Paragraph>Hello Sidebar Component</Paragraph>;
 };
 
