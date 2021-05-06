1329d7ccb6a0312190ecca186f125ba3d99799b0
Joe Tustin <J.Tustin@gmail.com>
2021-05-06 13:37:59 -0600

    adding in btc price func, replace api endpoint

--- src/components/BTCPrice.tsx
+++ src/components/BTCPrice.tsx
@@ -7,87 +7,10 @@ import axios from 'axios'
 interface SidebarProps {
   sdk: SidebarExtensionSDK;
 }
-const CONTENT_FIELD_ID = 'body';
 
+const BTCPrice = (props: SidebarProps) => {
 
-
-// async function btcPrice2() {
-//     let curRate
-//     let curTime
-
-//     try {
-//       const response = await axios.get('https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=600798C7-84CB-4842-B476-3D4427243952')
-//       curRate = response.data.rate
-//       curTime = response.data.time
-//     //   curRate = response.data.rates[3326].rate
-//     //   curTime = response.data.rates[3326].time
-//     //   console.log(response);
-//       console.log(curRate, "curRate")
-//       console.log(curTime, "curTime")
-//     } catch (error) {
-//       console.error(error);
-//     }
-
-//     // return '$54,454'
-
-//     curStats = "yes"
-
-
-//     return {
-//         curRate, 
-//         curTime
-//     }
-
-//   }
-
-
-
-
-// const btcPrice = () => {
-//     let curRate
-//     let curTime
-
-//     axios.get('https://rest.coinapi.io/v1/exchangerate/BTC?apikey=600798C7-84CB-4842-B476-3D4427243952')
-//         .then(function (response) {
-//             curRate = response.data.rates[3326].rate
-//             curTime = response.data.rates[3326].time
-//             // handle success
-//             // console.log("Hello");
-//             // console.log(response);
-//             // console.log(response.data.rates[3326]);
-//             // return {
-//             //     curRate, curTime
-//             // }
-//         })
-//         .catch(function (error) {
-//             // handle error
-//             console.log(error);
-//         })
-//         .then(function () {
-//             // always executed
-//     });
-
-//         console.log(curTime, "curTime")
-//         console.log(curRate, "curRate")
-
-//     // return [
-//     //     curRate, 
-//     //     curTime
-//     // ]
-//     // return '$54,454'
-//     return curRate
-// }
-
-
-
-const Sidebar = (props: SidebarProps) => {
-  const { sdk } = props;
-
-  const contentField = sdk.entry.fields[CONTENT_FIELD_ID];
-
-//   const [blogText, setBlogText] = useState(contentField.getValue());
   const [btcStats, setBtcStats] = useState({'curRate': 0, "curTime": ''});
-//   const [btcStats, setBtcStats] = useState({});
 
     useEffect(() => {
         let curRate
@@ -114,19 +37,6 @@ const Sidebar = (props: SidebarProps) => {
       </Note>
     </>
   );
-//   // Render the metrics with Forma36 components
-//   return (
-//     <>
-//       <Note style={{ marginBottom: '12px' }}>
-//         Metrics for your blog post:
-//         <List style={{ marginTop: '12px' }}>
-//           <ListItem>Word count: {stats.words}</ListItem>
-//           <ListItem>Reading time: {stats.text}</ListItem>
-//         </List>
-//       </Note>
-//     </>
-//   );
-};
+}
 
-
-export default Sidebar;
+export default BTCPrice;
